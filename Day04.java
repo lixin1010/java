c4bfe388992e202927a227420d2b18fd4871357d
lixin <xinli_2014@163.com>
2024-11-18 09:52:07 +0800

    day04

--- Day04.java
+++ Day04.java
@@ -1,5 +1,41 @@
 public class Day04 {
     public static void main(String[] args) {
+        Person_1118 lx = new Person_1118();
+//        lx.setBirth(2001);
+//        System.out.println(lx.getAge());
+        lx.setNameAndAge("lx",33);
+        System.out.println(lx.getName());
+        System.out.println(lx.getAge());
+    }
+}
 
+class Person_1118{
+    private String name;
+//    private int birth;
+    private int age;
+//    public void setBirth(int birth){
+//        this.birth = birth;
+//    }
+    private void setAge(int age){
+        this.age = age;
+    }
+    private void setName(String name){
+        this.name = name;
+    }
+//    public int getAge(){
+//        return calcAge(2024);
+//    }
+//    private int calcAge(int currentYear){
+//        return currentYear-this.birth;
+//    }
+    public void setNameAndAge(String name,int age){
+        setAge(age);
+        setName(name);
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
     }
 }
