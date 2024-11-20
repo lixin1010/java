73c3c4ec1b2b0348cff4f20ab62104aa0b52ff9b
lixin <xinli_2014@163.com>
2024-11-20 17:28:50 +0800

    day05_2

--- Day05.java
+++ Day05.java
@@ -1,27 +1,26 @@
 public class Day05 {
     public static void main(String[] args) {
 
-        People p = new People();
-        People p1 = new People("lx");
-        People p2 = new People("lixin",18);
+        People p = new People("xiaoming",22);
+//        People p1 = new People("lx");
+//        People p2 = new People("lixin",18);
+        System.out.println(p.getAge());
+        System.out.println(p.getName());
     }
 }
 
 class People{
     private String name;
     private int age;
-
-    public People(String name, int age) {
+    public People(String name,int age){
         this.name = name;
         this.age = age;
     }
+    public String getName(){
+        return name;
 
-    public People(String name) {
-        this(name, 18); // 调用另一个构造方法Person(String, int)
     }
-
-    public People() {
-        this("Unnamed"); // 调用另一个构造方法Person(String)
+    public int getAge(){
+        return age;
     }
-
 }
